a44cd481139b2c0b2016ad3dbb0763134190d8d6
Jeff Yahin Jørgensen <Jeff@Jeffs-MacBook-Pro.local>
2017-01-09 20:45:25 +0100

    Initial Commit

---
+++ GlobeGuides/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlobeGuides
+//
+//  Created by Jeff Yahin Jørgensen on 09/01/2017.
+//  Copyright © 2017 Jeff Yahin Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
